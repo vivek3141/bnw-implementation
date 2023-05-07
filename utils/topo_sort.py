d633c32c483474ec8eb162a2ef6904191eea1299
Young-Jin Park <youngjinpark20@gmail.com>
2023-05-06 23:58:39 -0700

    Maybe topo_sort

--- utils/topo_sort.py
+++ utils/topo_sort.py
@@ -1,4 +1,19 @@
 from utils.graph import Graph
 
-def topo_sort(G: Graph) -> list:
-    # return topological ordering of vertices of G
+def topo_sort(graph: Graph):
+    visited = set()
+    stack = []
+
+    def dfs(node):
+        visited.add(node)
+        for neighbor, _ in graph.get_adj(node):
+            if neighbor not in visited:
+                dfs(neighbor)
+        stack.append(node)
+
+    for node in graph.get_vertices():
+        if node not in visited:
+            dfs(node)
+
+    return stack[::-1]
+
