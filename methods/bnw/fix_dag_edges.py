4939884ec238690e8bbb76661f6b5bd4e1c8a867
Young-Jin Park <youngjinpark20@gmail.com>
2023-05-06 23:55:56 -0700

    Maybe fix fix_dag_edges

--- methods/bnw/fix_dag_edges.py
+++ methods/bnw/fix_dag_edges.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from utils.graph import Graph
 from utils.topo_sort import topo_sort
 from utils.bnw_utils import create_scc_dag
@@ -5,28 +6,32 @@ from utils.bnw_utils import create_scc_dag
 def fix_dag_edges(G: Graph, SCCs: list): # maybe change SCCs to be dictionary?
     G_scc_dag = create_scc_dag(G, SCCs)
     topo_ordering = topo_sort(G_scc_dag) # topo_ordering will store labels to SCCs in topo order
-    
-    # also figure out how to do vertex to SCC mappings
-    vertex_to_scc = # TODO
-    
+
+    vertex_to_scc = defaultdict(int)
+    for i in range(len(SCCs)):
+        scc = SCCs[i]
+        for node in scc:
+            vertex_to_scc[node] = i
+
+
     mu = {j: 0 for j in topo_ordering}
     for u in G.get_vertices():
         for v, weight in G.get_adj(u):
             scc_u = vertex_to_scc[u]
             scc_v = vertex_to_scc[v]
-            
+
             if scc_u != scc_v and weight < mu[topo_ordering[scc_v]]:
                 mu[topo_ordering[scc_v]] = weight
-    
+
     M = 0 # mu[topo_ordering[0]] = 0
     phi = {v: 0 for v in G.get_vertices()}
     for j in topo_ordering[1:]:
         M += mu[j] # M_j = sum_{k≤j} mu[k]
         for v in SCCs[j]: # all v ∈ V_j
             phi[v] = M
-    
+
     return phi
-        
-            
-            
-    
+
+
+
+
